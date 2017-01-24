1ad6a60f7680610f13900332b0da2289307f7c6d
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-01-24 16:12:15 +0100

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -16,15 +16,26 @@ declare module "mobx-form-reactions" {
     value: any;
     constructor(name: string, options?: FieldOptions);
     readonly valid: boolean;
+    init(name: string, options: FieldOptions): void;
     setValue(value: any): Promise<boolean>;
     hydrate(value: any): void;
-    private startValidation(value);
+    startValidation(value: any): Promise<boolean>;
+    stopValidation(errors: string[]): void;
+  }
+
+  export interface FieldCache {
+    [key: string]: Field;
   }
 
   export class Form {
     fields: Field[];
     constructor(fields: Field[]);
     readonly valid: boolean;
+    init(): void;
     addFields(...fields: Field[]): void;
+  }
+
+  export class SimpleForm extends Form {
+    constructor(model: Object, fields?: Field[]);
   }
 }
