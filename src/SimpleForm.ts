0953803f19b1822087f61d6e01ff4d4a82001291
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-01-25 20:27:32 +0100

    Restructure code

--- src/SimpleForm.ts
+++ src/SimpleForm.ts
@@ -6,12 +6,24 @@ export default class SimpleForm extends Form {
   constructor(model: Object, fields?: Field[]) {
     super(fields);
 
+    this.observeModel(model);
+  }
+
+  observeModel(model: Object) {
     observe(model, (change: any) => {
       const key = change.name;
       const next = change.object[change.name];
 
-      if (this.fields[key]) {
-        this.fields[key].setValue(next);
+      console.log("change", change);
+
+      const field = this.fields[key];
+
+      if (field) {
+        if (field instanceof Field) {
+          (field as Field).setValue(next);
+        } else if (field instanceof Form) {
+          this.observeModel(field);
+        }
       }
     });
   }
