16dc8a26eb2eca1dc731db49aeaa0d8de19c94bc
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-01-27 08:35:32 +0100

    Export validations

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Field } from "./Field";
 export { default as Form } from "./FormGroup";
 export { default as SimpleForm } from "./SimpleForm";
 export * from "./utils";
+export * from "./validations";
